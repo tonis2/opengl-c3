4c2dbb2b1b2057d38111e5d0f261e92c692b036c
tanton <tanton@paysure.solutions>
2022-07-17 19:39:32 +0300

    Parsing works better

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # opengl-c3
+
 Automatic opengl builds for C3
+
+Clone repo with --recursive for OPENGL config, and then run `dart run main.dart`;
--- main.dart
+++ main.dart
@@ -7,55 +7,167 @@ class EnumValue {
   const EnumValue(this.value, this.name);
 }
 
+class Param {
+  String name;
+  String type;
+  Param(this.type, this.name);
+
+  String toString() {
+    return toC3_Type(type) + " " + name;
+  }
+}
+
 class Command {
-  final String response;
+  final String returnType;
   final String name;
-  final Map<String, String> params;
-  const Command(this.response, this.name, this.params);
+  final List<Param> params;
+  const Command(this.returnType, this.name, this.params);
 }
 
-parseEnums(XmlDocument document, List<EnumValue> list) {
-  final enums = document.findAllElements('enums');
-
-  enums.map((XmlElement node) => node).forEach((node) {
-    // var comment = node.getAttribute("comment");
-
-    node.children.forEach((child) {
-      String value = child.getAttribute("value");
-      if (value == null) return;
-      list.add(EnumValue(int.parse(child.getAttribute("value")), child.getAttribute("name").replaceAll("GL_", "")));
-    });
-  });
+enum C_types {
+  GLsizei,
+  GLuint,
+  GLfloat,
+  GLclampf,
+  GLenum,
+  GLuint64EXT,
+  GLubyte,
+  GLdouble,
+  GLchar,
+  GLshort,
+  GLboolean,
+  GLint,
+  GLuint64
 }
 
-parseCommands(XmlDocument document, List<Command> list) {
-  final commands = document.findAllElements('command');
+extension C3Type on C_types {
+  String get name {
+    switch (this) {
+      case C_types.GLsizei:
+        return 'isize';
+      case C_types.GLuint:
+        return 'uint';
+      case C_types.GLfloat:
+        return 'float';
+      case C_types.GLclampf:
+        return 'float';
+      case C_types.GLenum:
+        return 'int';
+      case C_types.GLuint64EXT:
+        return 'int';
+      case C_types.GLubyte:
+        return 'uint';
+      case C_types.GLdouble:
+        return 'double';
+      case C_types.GLchar:
+        return 'char';
+      case C_types.GLshort:
+        return 'uint';
+      case C_types.GLboolean:
+        return 'bool';
+      default:
+        return null;
+    }
+  }
+}
 
-  commands.map((XmlElement node) => node).forEach((node) {
-    var proto = node.getElement("proto");
-    var fnName = node.getAttribute("name");
-    if (proto != null) print(proto.text.split(" "));
+String toC3_Type(String value) {
+  switch (value) {
+    case "GLsizei":
+      return 'isize';
+    case "GLuint":
+      return 'uint';
+    case "GLfloat":
+      return 'float';
+    case "GLclampf":
+      return 'float';
+    case "GLenum":
+      return 'int';
+    case "GLuint64EXT":
+      return 'int';
+    case "GLubyte":
+      return 'uint';
+    case "GLdouble":
+      return 'double';
+    case "GLchar":
+      return 'char';
+    case "GLshort":
+      return 'uint';
+    case "GLboolean":
+      return 'bool';
+    case "GLint":
+      return "int";
+    case "GLuint64":
+      return "int";
+    default:
+      // print(value);
+      return value;
+  }
+}
 
-    // print(fnName);
-    // node.children.forEach((child) {
-    //   String value = child.getAttribute("value");
-    //   if (value == null) return;
-    //   list.add(EnumValue(int.parse(child.getAttribute("value")), child.getAttribute("name").replaceAll("GL_", "")));
-    // });
-  });
+List<EnumValue> parseEnums(XmlDocument document) {
+  return document
+      .findAllElements('enum')
+      .map((XmlElement node) {
+        // var comment = node.getAttribute("comment");
+        String value = node.getAttribute("value");
+        if (value == null) return null;
+        return EnumValue(int.parse(node.getAttribute("value")), node.getAttribute("name"));
+      })
+      .where((element) => element != null)
+      .toList();
+}
+
+List<Command> parseCommands(XmlDocument document) {
+  return document
+      .findAllElements('command')
+      .map((XmlElement node) {
+        var proto = node.getElement("proto");
+        if (proto != null) {
+          List<Param> params = node.findAllElements("param").map((XmlElement value) {
+            var text = value.text.split(" ");
+            var type = value.getElement("ptype");
+            if (type == null)
+              return Param(text[0], text[text.length - 1]);
+            else
+              return Param(type.text, text[text.length - 1]);
+          }).toList();
+
+          var fnName = proto.text.split(" ");
+          return Command(fnName[0], fnName[1], params);
+        }
+      })
+      .where((element) => element != null)
+      .toList();
+}
+
+void write_C3file(List<Command> commands, List<EnumValue> enums) async {
+  var file = File('./build/opengl.c3');
+
+  String fnData = "// Functions \n \n" +
+      commands.map((element) {
+        return "extern fn " +
+            toC3_Type(element.returnType) +
+            " " +
+            element.name +
+            "(" +
+            element.params.map((e) => e.toString()).join(", ") +
+            ");";
+      }).join("\n");
+
+  String constants = "// Constants \n \n" +
+      enums.map((element) {
+        return "const " + element.name + " = " + "0x" + element.value.toRadixString(16).toString() + ";";
+      }).join("\n");
+
+  file.writeAsStringSync(fnData + "\n \n" + constants);
 }
 
 void main() {
-  List<EnumValue> enumList = [];
-  List<Command> commandList = [];
-
   final file = new File('dependencies/gl/xml/gl.xml');
   final document = XmlDocument.parse(file.readAsStringSync());
+  List<Command> commandList = parseCommands(document);
+  List<EnumValue> enumList = parseEnums(document);
 
-  // parseEnums(document, enumList);
-  parseCommands(document, commandList);
-
-  // enumList.forEach((element) {
-  //   print("const " + element.name + " = " + element.value.toString());
-  // });
+  write_C3file(commandList, enumList);
 }
